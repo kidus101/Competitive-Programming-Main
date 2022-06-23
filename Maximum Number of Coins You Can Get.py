a9bb122567e8565783955bf9f88016ed292e1fa8
kidus <69918484+kidus101@users.noreply.github.com>
2022-06-23 14:56:44 +0300

    Update Maximum Number of Coins You Can Get.py

--- Maximum Number of Coins You Can Get.py
+++ Maximum Number of Coins You Can Get.py
@@ -1,17 +1,19 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         sortedPiles =sorted(piles)
-        pileLength = len(piles)
+        
         totalCoins =0
+        pileLength = len(piles)
         
         i = pileLength - 2
         j = 0
         
-        for(j < pileLength/3; j++;i-=2 ):
+        while(j < pileLength/3 ):
+            j += 1
             totalCoins += sortedPiles[i]
+            i-=2
         return totalCoins
-        
-      #Time complexity:O(n)
-    #Space Complexity:O(1)       
-        
     
+    #Time complexity:O(n)
+    #Space Complexity:O(1)
+       
